5c736af053ace9371e5f342c5d25a6d3bbb788bf
rashmi rangari <rashmirangari.p@gmail.com>
2021-03-24 00:51:45 +0530

    added basic functionalilty of textEditor

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * TODO: Update the text in the "Formatted Text" section as a user types in the textarea.
+ * TODO: Add a .bold, .italic classed to "Formatted Text" when the aapropriate button is clicked
+ * TODO: Add an .underline class to "Formatted Text" when Unerline button is clicked.
+ * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked.
+ */
+
+//Update the output text as a user types in the textarea
+function updateText(){
+    
+}
