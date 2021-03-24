741a1f9c59d3803239a8e9a99d9008446b0b81e6
rashmi rangari <rashmirangari.p@gmail.com>
2021-03-25 00:26:31 +0530

    Update the text in the Formatted Text section as a user types in the textarea.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea.
+ * DONE: Update the text in the "Formatted Text" section as a user types in the textarea.
  * TODO: Add a .bold, .italic classed to "Formatted Text" when the aapropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Unerline button is clicked.
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked.
@@ -7,5 +7,7 @@
 
 //Update the output text as a user types in the textarea
 function updateText(){
-    
+   // console.log("working");
+    let text = document.getElementById("text-input").value;
+    document.getElementById('text-output').innerText = text;
 }
