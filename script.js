73b876d9570aeec8babfa2d198c14e9b6bf49b71
rashmi rangari <rashmirangari.p@gmail.com>
2021-03-25 01:35:39 +0530

    Add a .bold, .italic classed to Formatted Text when the aapropriate button is clicked

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea.
- * TODO: Add a .bold, .italic classed to "Formatted Text" when the aapropriate button is clicked
+ * DONE: Add a .bold, .italic classed to "Formatted Text" when the aapropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Unerline button is clicked.
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked.
  */
@@ -11,3 +11,25 @@ function updateText(){
     let text = document.getElementById("text-input").value;
     document.getElementById('text-output').innerText = text;
 }
+
+// Toggle the bold class for ouptut text
+function makeBold(elem){
+   
+    elem.classList.toggle('active');
+    document.getElementById('text-output').classList.toggle('bold');
+}  
+
+function makeItalic(elem){
+    elem.classList.toggle('active');
+    document.getElementById('text-output').classList.toggle('italic');
+}
+
+// function makeUnderline(elem){
+//     elem.classList.toggle('active');
+//     let formatteText = document.getElementById('text-output').classList.toggle('underline');
+//     if(formatteText.classList.contains('underline')){
+//         formatteText.classList.remove('underline');
+//     } else{
+//         formatteText.classList.add('underline');
+//     }
+// }
