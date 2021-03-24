d0926cc9b94f9db29153cecd9fefbdebb9bbbe7e
rashmi rangari <rashmirangari.p@gmail.com>
2021-03-25 02:21:09 +0530

    changes in functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea.
  * DONE: Add a .bold, .italic classed to "Formatted Text" when the aapropriate button is clicked
  * DONE: Add an .underline class to "Formatted Text" when Unerline button is clicked.
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked.
+ * DONE: Toggle the align style for "Formatted Text" when the appropriate button is clicked.
  */
 
 //Update the output text as a user types in the textarea
